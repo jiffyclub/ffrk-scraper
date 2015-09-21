c14eaadf63e5bdb8707f70dc807867498b83cbae
Matt Davis <jiffyclub@gmail.com>
2015-09-21 14:46:04 -0700

    echo step in genall.fish

--- genall.fish
+++ genall.fish
@@ -1,7 +1,9 @@
 for lang in en jp
     for level in 50 65
+        echo 'characters' $lang $level
         python char-scraper.py $lang $level
     end
 
+    echo 'items' $lang
     python item-scraper.py $lang
 end
